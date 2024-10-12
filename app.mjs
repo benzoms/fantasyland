ae75ed059447dd5f8a72052d80985ec78b51403f
Ben Zhang <ben@theclarityteam.net>
2024-10-11 22:39:31 -0700

    updated for oct12

--- app.mjs
+++ app.mjs
@@ -194,6 +194,7 @@ app.get('/old-eggs', async(req, res) => {
 
 
 const potdslist = [
+  { day: 'oct12', caption: 'October 12 -  Us sittin\' on ya driveway!', src: '' },
   { day: 'oct11', caption: 'October 11 -  Us on a breakfast date at Kona!', src: '' },
   { day: 'oct10', caption: 'October 10 -  Us "falling" in love!', src: '' },
   { day: 'oct9', caption: 'October 9 -  Us at Meydenbauer Beach Park!', src: '' },
