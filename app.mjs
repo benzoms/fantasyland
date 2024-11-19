1be9bc73366fda0160ee0e9d9b7737d151e64921
Ben Zhang <ben@theclarityteam.net>
2024-11-18 23:33:36 -0800

    updated for nov19

--- app.mjs
+++ app.mjs
@@ -201,6 +201,7 @@ app.get('/old-eggs', async(req, res) => {
 
 
 const potdslist = [
+  { day: 'nov19', caption: 'November 19 -  Us post pumpy-patch!!!', src: '' },
   { day: 'nov18', caption: 'November 18 -  Our shadows outside of Laura\'s apartment!!', src: '' },
   { day: 'nov17', caption: 'November 17 -  Us and our son in another family photo <3!', src: '' },
   { day: 'nov16', caption: 'November 16 -  Us through DJ Bohan\'s eyes!!!', src: '' },
